9f7544d87dfe8f4224e17cb83679bc94a2ad7c0b
mydearxym <mydearxym@qq.com>
2020-12-22 15:10:10 +0800

    refactor: constant names

--- src/constant.js
+++ src/constant.js
@@ -16,70 +16,25 @@ export const DEFAULT_LABEL = "标签";
 export const SORT_DEFAULT = "sort_default";
 export const SORT_UP = "sort_up";
 export const SORT_DOWN = "sort_down";
-export const SORT_ENUM = ["sort_default", "sort_up", "sort_down"];
+export const SORT_ENUM = [SORT_DEFAULT, SORT_UP, SORT_DOWN];
 
 export const SORT_ORDER = {
-  sort_up: {
+  [SORT_UP]: {
     green: 0,
     warn: 1,
     red: 2,
     default: 3,
   },
-  sort_down: {
+  [SORT_DOWN]: {
     red: 0,
     warn: 1,
     green: 2,
     default: 3,
   },
-  sort_default: {
+  [SORT_DEFAULT]: {
     warn: 0,
     red: 1,
     green: 2,
     default: 3,
   },
 };
-
-// const LN = {
-//   ORDERED_LIST = "ordered_list",
-//   UNORDERED_LIST = "unordered_list",
-//   CHECKLIST = "checklist",
-//   ORG_MODE = "org_mode",
-//   SORT = "sort",
-//   // ---
-//   LABEL_TYPE = {
-//     RED = "red",
-//     GREEN: "green",
-//     WARN: "warn",
-//     DEFAULT: "default",
-//   },
-
-//   DEFAULT_LABEL: "标签",
-//   //
-//   SORT_DEFAULT: "sort_default",
-//   SORT_UP: "sort_up",
-//   SORT_DOWN: "sort_down",
-//   SORT_ENUM: ["sort_default", "sort_up", "sort_down"],
-
-//   SORT_ORDER: {
-//     sort_up: {
-//       green: 0,
-//       warn: 1,
-//       red: 2,
-//       default: 3,
-//     },
-//     sort_down: {
-//       red: 0,
-//       warn: 1,
-//       green: 2,
-//       default: 3,
-//     },
-//     sort_default: {
-//       warn: 0,
-//       red: 1,
-//       green: 2,
-//       default: 3,
-//     },
-//   },
-// };
-
-// export default LN;
